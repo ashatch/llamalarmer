4934ff79961bc6ba5bbb978c5962a878c8f21f38
Andrew Hatch <ahatch@amplience.com>
2019-11-22 13:06:41 +0000

    switch animates up and down

--- src/switch.ts
+++ src/switch.ts
@@ -4,12 +4,20 @@ export default class Switch {
   sprite: Phaser.GameObjects.Sprite;
   
   constructor(scene: Phaser.Scene, x: number, y: number) {
+    scene.anims.create({
+      key: "switch-up-and-down",
+      frames: scene.anims.generateFrameNumbers("switch", { start: 0, end: 1 }),
+      frameRate: 3,
+      repeat: -1
+    });
+
+
     this.sprite = scene.add.sprite(x, y, 'switch');
     this.sprite.setScale(2,2);
   }
 
   update() {
-    
+    this.sprite.anims.play("switch-up-and-down", true);
   }
 
   destroy() {
